eec15e76c8ec0ef13d89e9542bcf4a7110450e42
tymwitko <tymon.kobylecki@gmail.com>
2022-02-06 01:37:07 +0100

    initial prototype

---
+++ app/src/main/java/com/tymwitko/bogitrip/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.tymwitko.bogitrip
+
+//import com.niels_ole.customtileserver.R
+
+//import jdk.incubator.jpackage.internal.Arguments.CLIOptions.context
+import android.Manifest
+import android.Manifest.permission
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.test.core.app.ApplicationProvider
+import com.tymwitko.bogitrip.databinding.ActivityMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.config.Configuration.getInstance
+
+
+class MainActivity : AppCompatActivity() {
+//    companion object {
+//
+//    }
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+//    private lateinit var map : MapView
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
+
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+        // Make sure actions in the ActionBar get propagated to the NavController
+        setupActionBarWithNavController(navController)
+
+
+        setContentView(binding.root)
+//        setContentView(R.layout.activity_main)
+
+//        findViewById<TextView>(R.id.textView).movementMethod = LinkMovementMethod.getInstance()
+
+    }
+
+//    /**
+//     * Enables back button support. Simply navigates one element up on the stack.
+//     */
+//    override fun onSupportNavigateUp(): Boolean {
+//        return navController.navigateUp() || super.onSupportNavigateUp()
+//    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val permissionsToRequest = ArrayList<String>()
+        var i = 0
+        while (i < grantResults.size) {
+            permissionsToRequest.add(permissions[i])
+            i++
+        }
+        if (permissionsToRequest.size > 0) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE)
+        }
+    }
+
+
+//    private fun requestPermissionsIfNecessary(permissions: Array<String>) {
+//        var permissionsToRequest: Array<String> = emptyArray()
+//        for (permission in permissions) {
+//            if (ContextCompat.checkSelfPermission(this, Manifest.permission)
+//                != PackageManager.PERMISSION_GRANTED) {
+//                // Permission is not granted
+//                permissionsToRequest.append(Manifest.permission);
+//            }
+//        }
+//        if (permissionsToRequest.size() > 0) {
+//            ActivityCompat.requestPermissions(
+//                this,
+//                permissionsToRequest.toArray("0"),
+//                REQUEST_PERMISSIONS_REQUEST_CODE)
+//        }
+//    }
+}
---
+++ app/src/main/java/com/tymwitko/bogitrip/StarterMapFragment.kt
@@ -0,0 +1,285 @@
+package com.tymwitko.bogitrip
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.location.*
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.tymwitko.bogitrip.databinding.FragmentStarterMapBinding
+import org.osmdroid.api.IGeoPoint
+import org.osmdroid.api.IMapView
+import org.osmdroid.config.Configuration
+import org.osmdroid.events.MapListener
+import org.osmdroid.events.ScrollEvent
+import org.osmdroid.events.ZoomEvent
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.*
+import org.osmdroid.views.overlay.compass.CompassOverlay
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import java.util.*
+import java.util.zip.DeflaterOutputStream
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StarterMapFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StarterMapFragment : Fragment(), View.OnClickListener, View.OnLongClickListener {
+//    private var _binding: FragmentStarterMapBinding? = null
+    private lateinit var map : MapView
+    private lateinit var v : View
+    private var minRange: Double = 0.0
+    private var maxRange: Double = 0.0
+//
+//    // This property is only valid between onCreateView and
+//    // onDestroyView.
+//    private val binding get() = _binding!!
+//
+//    private lateinit var mLastLocation : Location
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Configuration.getInstance().userAgentValue = context?.packageName
+        //important! set your user agent to prevent getting banned from the osm servers
+
+//        val policy = ThreadPolicy.Builder().permitAll().build()
+//        StrictMode.setThreadPolicy(policy)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        v = inflater.inflate(R.layout.fragment_starter_map, null)
+        val btnRandom = v.findViewById<View>(R.id.btnRandom)
+        val btnRoute = v.findViewById<View>(R.id.btnRoute)
+        btnRandom.setOnClickListener(this)
+        btnRoute.setOnClickListener(this)
+//        textViewCurrentLocation = v.findViewById<View>(R.id.textViewCurrentLocation)
+        map = v.findViewById<View>(R.id.mapview) as MapView
+        map.addMapListener(object : MapListener {
+            override fun onScroll(event: ScrollEvent): Boolean {
+                Log.i(
+                    IMapView.LOGTAG,
+                    System.currentTimeMillis().toString() + " onScroll " + event.x + "," + event.y
+                )
+                //Toast.makeText(getActivity(), "onScroll", Toast.LENGTH_SHORT).show();
+                return true
+            }
+
+            override fun onZoom(event: ZoomEvent): Boolean {
+                Log.i(
+                    IMapView.LOGTAG,
+                    System.currentTimeMillis().toString() + " onZoom " + event.zoomLevel
+                )
+                return true
+            }
+        })
+        Log.d("FATAL EXCEPTION: main", "po listenerze")
+
+//        getView()?.findViewById<TextView>(R.id.textView)?.movementMethod = LinkMovementMethod.getInstance()
+//        map = MapView(requireView().findViewById(R.id.mapview))
+//        map = MapView(requireContext())
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
+        map.setUseDataConnection(true)
+        map.controller.setZoom(14.0)
+        map.controller.setCenter(GeoPoint(52.0, 21.0))
+
+//        val compassOverlay = CompassOverlay(context, InternalCompassOrientationProvider(context), map)
+//        compassOverlay.enableCompass()
+//        map.overlays.add(compassOverlay)
+
+        val rotationGestureOverlay = RotationGestureOverlay(map)
+        rotationGestureOverlay.isEnabled
+        map.setMultiTouchControls(true)
+        map.overlays.add(rotationGestureOverlay)
+
+        val copyrightOverlay = CopyrightOverlay(requireContext())
+        copyrightOverlay.setAlignBottom(true)
+        copyrightOverlay.setAlignRight(true)
+        map.overlays.add(copyrightOverlay)
+
+
+
+
+        // Note, "context" refers to your activity/application context.
+        // You can simply do resources.displayMetrics when inside an activity.
+        // When you aren't in an activity class, you will need to have passed the context
+        // to the non-activity class.
+        val dm : DisplayMetrics? = context?.resources?.displayMetrics
+        val scaleBarOverlay = ScaleBarOverlay(map)
+        scaleBarOverlay.setCentred(true)
+        //play around with these values to get the location on screen in the right place for your application
+        scaleBarOverlay.setScaleBarOffset(dm!!.widthPixels / 2, 10)
+        map.overlays.add(scaleBarOverlay);
+
+        //icons listener
+        //your items
+        val items = ArrayList<OverlayItem>()
+        items.add(OverlayItem("Zero-zero", "Środek niczego lol", GeoPoint(0.0, 0.0)))
+
+        //the overlay
+        @Suppress("DEPRECATION") val overlay = ItemizedOverlayWithFocus(items, object: ItemizedIconOverlay.OnItemGestureListener<OverlayItem>{
+            override fun onItemSingleTapUp(index:Int, item:OverlayItem):Boolean {
+                //do something
+                return true
+            }
+            override fun onItemLongPress(index:Int, item:OverlayItem):Boolean {
+                return false
+            }
+        }, context)
+        overlay.setFocusItemsOnTap(true);
+
+        map.overlays.add(overlay)
+
+        // Acquire a reference to the system Location Manager
+        Log.d("FATAL EXCEPTION: main", "przed managerem")
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        Log.d("FATAL EXCEPTION: main", "po locationmanagerze")
+
+        val provider = GpsMyLocationProvider(requireContext())
+        provider.addLocationSource(LocationManager.GPS_PROVIDER)
+        provider.addLocationSource(LocationManager.NETWORK_PROVIDER)
+        provider.locationUpdateMinDistance = 100f
+        provider.locationUpdateMinTime = 10000
+
+
+        // Define a listener that responds to location updates
+
+        // Define a listener that responds to location updates
+        val locationListener: LocationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location) {
+                // Called when a new location is found by the network location provider.
+                Log.d("TAG", "Location: ${location.latitude}")
+            }
+
+            override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
+                Log.d("TAG", "Location status: $provider $status")
+            }
+
+            override fun onProviderEnabled(provider: String) {
+                Log.d("TAG", "Provider enabled $provider")
+            }
+
+            override fun onProviderDisabled(provider: String) {
+                Log.d("TAG", "Provider disabled $provider")
+            }
+        }
+
+        // Register the listener with the Location Manager to receive location updates
+
+        // Register the listener with the Location Manager to receive location updates
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("TAG", "Location access granted")
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                10000,
+                100f,
+                locationListener
+            )
+        }
+        val myLocationOverlay = MyLocationNewOverlay(provider, map)
+        if (!myLocationOverlay.enableMyLocation()) {
+            Log.d("TAG", "Location not enabled")
+        }
+
+        myLocationOverlay.isDrawAccuracyEnabled = true
+        val icon: Bitmap = BitmapFactory.decodeResource(resources,
+            android.R.drawable.presence_online
+        )
+        myLocationOverlay.setPersonIcon(icon)
+
+        myLocationOverlay.runOnFirstFix(Runnable { // never reaches this point
+            Log.d("TAG", "runOnFirstFix")
+            if (myLocationOverlay.myLocation == null) {
+                Log.d("TAG", "Location nott retrieved")
+            }
+        })
+        Log.d("FATAL EXCEPTION: main", "w location listenerze")
+        map.overlays.add(myLocationOverlay)
+
+        if (myLocationOverlay.myLocation == null) {
+            // could be too early
+            Log.d("TAG", "Location not retrieved")
+        }
+        Log.d("FATAL EXCEPTION: main", "przed returnem")
+        return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("FATAL EXCEPTION: main", "view created")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment StarterMapFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            StarterMapFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0 == v.findViewById(R.id.btnRandom)){
+            generateRandom(minRange, maxRange)
+        }
+    }
+
+    private fun generateRandom(minRange: Double, maxRange: Double) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLongClick(p0: View?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
